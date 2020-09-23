69e33270daf3b6e772942625cfe1cdee165912e9
ergifachri <ergifachri@gmail.com>
2020-09-23 21:46:31 +0700

    Edit kidzbarn https

--- src/components/utils/misc.js
+++ src/components/utils/misc.js
@@ -1,5 +1,5 @@
 // SERVER ROUTES
-export const API_URL = 'http://13.212.48.176:3003';
+export const API_URL = 'https://kidzbarn.id:444';
 export const USER_SERVER = `${API_URL}/api/v1/kidzbarn/users`;
 export const AUTH_SERVER = `${API_URL}/api/v1/kidzbarn/auth`;
 export const PRODUCT_SERVER = '/api/product/';
